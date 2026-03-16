a103b1f5d001634fbd238e4138b471c7661fa435
MALIHE <maheguz16@hotmail.com>
2026-03-15 20:59:59 -0600

    Proyecto Evaluador de Notas en JavaScript

---
+++ Documents/DEV.F/Modulo_3/Modulo_3_Intro_JavaScript/Evaluador Notas/EvaluadorNotas.js
@@ -0,0 +1,15 @@
+let nota = 85;
+console.log("La nota del estudiante es:", nota);
+
+if (nota >= 90) {
+    console.log("Excelente");
+}
+else if (nota >= 75) {
+    console.log("Bien");
+}
+else if (nota >= 60) {
+    console.log("Suficiente");
+}
+else {
+    console.log("No aprobado");
+}
